250042176b89e0e88b4a029a69a02be704364518
mariacarolinafs <mariacarolina_fs@hotmail.com>
2019-11-18 14:13:01 -0200

    firebase init

--- src/pages/movie.js
+++ src/pages/movie.js
@@ -1,31 +1,29 @@
-
 function Movie(props) {
   let template = `
     <div class='movie'></div>
     <div class='theater'></div>
   `
   console.log(location.hash);
-  fetch (movieUrl+ location.hash.substring(1))
+  fetch(movieUrl + location.hash.substring(1))
     .then(response => response.json())
     .then(data => {
       let movieData = data.results[0]
-      
+
       console.log(data.results[0]);
       // let moviePoster = data.results[0].poster_path
-    // console.log(props.Joker);
-    
-      
+      // console.log(props.Joker);
+
+
       document.querySelector('.movie').innerHTML = `<img src='https://image.tmdb.org/t/p/w200${movieData.poster_path}'><div><p>${movieData.title}</p><p>${movieData.overview}</p></div>`
       props.forEach(item => {
-        if (item.id === movieData.title){
-          for (let key in item.data()){
-            
-            document.querySelector('.theater').innerHTML +=` <h2>${key}</h2><p> ${item.data()[key]}</p>`
-          }         
+        if (item.id === movieData.title) {
+          for (let key in item.data()) {
+            document.querySelector('.theater').innerHTML += ` <h2>${key}</h2><p> ${item.data()[key]}</p>`
+          }
         }
       })
     })
-    return template 
+  return template
 }
 
 
