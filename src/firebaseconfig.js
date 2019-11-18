333e66f6679770424c8ba462621c8406af933f3a
jaquew <eng.jaquesilva@gmail.com>
2019-11-18 11:04:04 -0300

    first commit

---
+++ src/firebaseconfig.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyA7kT0BnJ0H7ykylGktr3BP_G96drEpKAI",
+    authDomain: "openhouse-lab.firebaseapp.com",
+    databaseURL: "https://openhouse-lab.firebaseio.com",
+    projectId: "openhouse-lab",
+    storageBucket: "openhouse-lab.appspot.com",
+    messagingSenderId: "1001561279511",
+    appId: "1:1001561279511:web:9eee8b60718693640e6052"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
