eb0bafd41ce09e4f412bfc74c3e0f9652e7b2a96
jaquew <eng.jaquesilva@gmail.com>
2019-11-18 15:09:05 -0300

    time and price added

--- src/pages/movie.js
+++ src/pages/movie.js
@@ -1,9 +1,11 @@
 
 function Movie(props) {
+  const date = new Date()
+  const today = date.getDay()
   let price = []
   let template = `
-    <div class='movie'></div>
-    <div class='theater'></div>
+    <section class='movie'></section>
+    <section class='theater'></section>
   `
   console.log(location.hash);
   fetch (movieUrl+ location.hash.substring(1))
@@ -11,33 +13,24 @@ function Movie(props) {
     .then(data => {
       let movieData = data.results[0];
       firebase.firestore().collection('cinema').get().then((snap) => {
-        snap.forEach(hora => {
-          // console.log(hora.id, hora.data());
-          
-
+        snap.forEach(hora => {      
           document.querySelector('.movie').innerHTML = `<img src='https://image.tmdb.org/t/p/w200${movieData.poster_path}'><div><p>${movieData.title}</p><p>${movieData.overview}</p></div>`
           props.forEach(item => {
             if (item.id === movieData.title){
               for (let key in item.data()){
                 if(key === hora.id){
                   let dado = item.data()[key]
-
-                  price.push({key, dado})
-                  
-                };
-                
+                  let horario = hora.data()[today]
+                  price.push([key, dado, horario])
+                };                
               }         
             }
           })
         })
-        console.log(price);
-        
-        
+        price.forEach(item => {
+          document.querySelector('.theater').innerHTML +=`<div><h2>${item[0]}</h2><p> ${item[1]}</p></div><div><p>${item[2]}</p><p>Botão Comprar</p></div>`
+        })    
       })
-      
-      // document.querySelector('.theater').innerHTML +=` <h2>${key}</h2><p> ${item.data()[key]}</p>`
-      
-      
     })
     return template 
 }
